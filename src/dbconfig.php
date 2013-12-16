dc1f9ebcbdb095206638463372eed4fcdc98fe35
Jonatan Karlsson <me@jonatankarlsson.se>
2013-12-16 23:03:11 +0100

    Delete dbconfig.php

--- src/dbconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- *  Defines database configs
- */
-define('DB_USER', 'jokd13');
-define('DB_PASSWORD', 'qv}K6[dL');
-define('DB_DSN', 'mysql:host=blu-ray.student.bth.se;dbname=jokd13;');
